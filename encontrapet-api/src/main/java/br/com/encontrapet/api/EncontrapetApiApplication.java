d90554eece4159a63a7c07c05268bd5fe995d225
PedroAthyrson <pedroathyrsondev@gmail.com>
2025-09-29 12:23:06 -0300

    first commit

---
+++ encontrapet-api/src/main/java/br/com/encontrapet/api/EncontrapetApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.encontrapet.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncontrapetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncontrapetApiApplication.class, args);
+	}
+
+}
---
+++ encontrapet-api/src/test/java/br/com/encontrapet/api/EncontrapetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.encontrapet.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncontrapetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
